4a9f68dbaa432e3a1f76c555246c22db34d33ecc
Bibek Bhandari <bhandaribibek731@gmail.com>
2023-03-06 21:35:17 +0545

    port

---
+++ musicplayer/music.js
@@ -0,0 +1,24 @@
+function myFunction() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
+
+
+const img = document.getElementById("languages")
+const getDropDown = document.getElementById("dropdownItem")
+
+
+
+function toggleDropDown() {
+  if (getDropDown.style.display === "block") {
+    getDropDown.style.display = "none"
+  } else {
+    getDropDown.style.display = "block"
+  }
+
+}
+
+
+
+
+
